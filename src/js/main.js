faee5999aa900c3380e2325004c38d56c6a5063f
shakhzodnematullaev <shakhzodnematullaev@gmail.com>
2021-05-26 14:43:33 +0500

    Add button function to FAQs

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,16 @@
 var burger = document.querySelector(".hamburger");
 var nav = document.querySelector(".wrapper");
+var button = document.querySelector(".sub-item__button");
+var text = document.querySelector(".sub-item__text");
+var strelka = document.querySelector(".faqs__sub-item");
 
 burger.addEventListener("click", function() {
     burger.classList.toggle("hamburger--active");
     nav.classList.toggle("wrapper--active");
+})
+
+button.addEventListener("click", function() { 
+    button.classList.toggle("sub-item__button--active");
+    text.classList.toggle("sub-item__text--active");
+    strelka.classList.toggle("faqs__sub-item--active");
 })
